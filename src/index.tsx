59e2a753a6753e3c6b37e4e0a8825b5aab66f121
Geoff Davis <gdavis@archermalmo.com>
2018-03-28 17:54:08 -0500

    change indentation to 2 space across VSCode, save for formatting

--- src/index.tsx
+++ src/index.tsx
@@ -3,39 +3,39 @@ import { ReactElement, ReactHTMLElement, ReactChildren } from "react";
 import { Breadcrumb as Crumb, RestProps } from "./types.d";
 
 const BreadcrumbNav = ({
-	children,
-	...restProps
+  children,
+  ...restProps
 }: {
-	children?: ReactChildren;
-	restProps?: RestProps;
+  children?: ReactChildren;
+  restProps?: RestProps;
 }) => (
-	<nav
-		className="react-aria-breadcrumbs"
-		aria-label="breadcrumbs"
-		role="navigation"
-		{...restProps}
-	>
-		<ol>{children}</ol>
-	</nav>
+  <nav
+    className="react-aria-breadcrumbs"
+    aria-label="breadcrumbs"
+    role="navigation"
+    {...restProps}
+  >
+    <ol>{children}</ol>
+  </nav>
 );
 
 const Breadcrumb = ({
-	children,
-	path,
-	title,
-	text,
-	current,
-	...restProps
+  children,
+  path,
+  title,
+  text,
+  current,
+  ...restProps
 }: Crumb) => (
-	<li {...restProps}>
-		<a
-			href={current ? "#current" : path}
-			title={current ? `current page` : title}
-			aria-current={current ? "page" : null}
-		>
-			{children ? children : text}
-		</a>
-	</li>
+  <li {...restProps}>
+    <a
+      href={current ? "#current" : path}
+      title={current ? `current page` : title}
+      aria-current={current ? "page" : null}
+    >
+      {children ? children : text}
+    </a>
+  </li>
 );
 
 export { Breadcrumb, BreadcrumbNav };
--- src/types.d.ts
+++ src/types.d.ts
@@ -3,12 +3,12 @@ import { ReactChildren } from "react";
 type RestProps = object;
 
 type Breadcrumb = {
-	children?: ReactChildren;
-	path: string;
-	title: string;
-	text?: string;
-	current?: boolean;
-	restProps?: RestProps;
+  children?: ReactChildren;
+  path: string;
+  title: string;
+  text?: string;
+  current?: boolean;
+  restProps?: RestProps;
 };
 
 export { Breadcrumb, RestProps };
